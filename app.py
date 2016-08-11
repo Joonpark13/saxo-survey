21094365f5b8f4bc3f1fff0877e8fca56b0fd509
Joon Park <JoonPark2017@u.northwestern.edu>
2016-08-11 03:12:16 -0500

    Allow debug mode if not running on pythonanywhere

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template
+from socket import gethostname # For checking if it's running on pythonanywhere
+
 app = Flask(__name__)
+#TODO
+# Currently just checks to see if app is running on pythonanywhere (intended
+# quick deploy location) and turns debug on if not true. Later refactor for
+# more robust deployment options.
+if 'liveconsole' not in gethostname():
+    app.config["DEBUG"] = True
 
 @app.route('/')
 def index():
